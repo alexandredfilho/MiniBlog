c2bda296181b3d39bf5995a79ddfc62717b0e50c
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-12-04 16:14:39 -0300

    Handle post details in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
 // CSS import
 import styles from "./Home.module.css"
 
+// Hooks
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+
+// Components
+import PostDetails from "../../components/PostDetails"
+
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const { documents: posts, loading } = useFetchDocuments("posts")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>See our most recent posts</h1>
+      <form onSubmit={handleSubmit} className={styles.searchForm}>
+        <input type="text" placeholder="Or search by tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div>
+        {loading && <p>Loading data...</p>}
+        {posts && posts.map((post) => (
+          <PostDetails key={post.id} post={post} />
+        ))}
+        {posts && posts.length === 0 && (
+          <div className={styles.noPosts}>
+            <p>There isn't posts created already!</p>
+            <Link to="/createPost" className="btn">Create your first post now!</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
